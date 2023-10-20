4da4222149b4f04bca055fbabcc4d15812805df2
owlks4 <115742200+owlks4@users.noreply.github.com>
2023-10-20 22:37:26 +0100

    Steps towards getting page to register updates when changing weeks

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,14 @@ class Adjacency {
   }
 }
 
+class Week {
+  constructor(year,week,title){
+    this.year=year;
+    this.week=week;
+    this.title=title;
+  }
+}
+
 class App extends React.Component {
 
   constructor(props){
@@ -19,8 +27,8 @@ class App extends React.Component {
     this.state = {
       territories: [],
       adjacencies: [],
-      year: props.year,
-      week: props.week,
+      year: 6,
+      week: 3,
       territoryLabelFontSize: "1em",
       weekTitle: "",
       dragging: false,
@@ -30,7 +38,8 @@ class App extends React.Component {
       mapAdjustTop: "40%",
       touchStartX: 0,
       touchStartY: 0,
-      fontSizeEm: 0.89
+      fontSizeEm: 0.89,
+      loadedWeeks: []
     }
   }
 
@@ -38,7 +47,8 @@ class App extends React.Component {
     
     if (this.state.dragging){
         this.setState({displayLeft: this.state.displayLeft + event.movementX,
-                                  displayTop: this.state.displayTop + event.movementY});
+                       displayTop: this.state.displayTop + event.movementY,
+                      territories: this.state.territories});
       //  app.updateCanvasPosition();
     }
   }
@@ -47,17 +57,14 @@ class App extends React.Component {
     if (this.state.dragging){
         let touch = event.touches[0];
         this.setState({displayLeft: this.state.displayLeft + (touch.clientX - this.state.touchStartX),
-                                  displayTop:  this.state.displayTop + (touch.clientY - this.state.touchStartY),
-                                  touchStartX: touch.clientX,
-                                  touchStartY: touch.clientY});
+                       displayTop:  this.state.displayTop + (touch.clientY - this.state.touchStartY),
+                       touchStartX: touch.clientX,
+                       touchStartY: touch.clientY});
        // app.updateCanvasPosition();
     }
   }
 
   componentDidMount(){
-    console.log(this.state);
-    this.getTerritoriesFromCSV(this.state.year,this.state.week);
-
     document.addEventListener('mousedown', () => {this.setState({dragging: true})});
     document.addEventListener("mousemove", (event) => {this.onMouseMove(event)});
     document.addEventListener("mouseup", () => {this.setState({dragging: false})});
@@ -65,6 +72,35 @@ class App extends React.Component {
     document.addEventListener("touchmove", this.onTouchMove);
     document.addEventListener("touchstart", ev => {this.setState({dragging: true}); let touch = ev.touches[0]; this.setState({touchStartX: touch.clientX, touchStartY: touch.clientY});});
     document.addEventListener("touchend",() => {this.setState({dragging: false});});
+
+    this.changeYear(6);
+    this.changeWeek(3);
+  }
+
+  changeYear(newYearNumber){
+
+    let newLoadedWeeks = [];
+
+    for (let i = 1; i < 30; i++){
+      fetch("./csv/y"+newYearNumber+"/territoryAssignments-wk"+i+".csv")
+      .then(response => response.text())
+      .then(text => {
+        if (text != ""){
+          let lines = text.split("\n");
+          newLoadedWeeks.push({title: "Week "+i+" - "+lines[0], weekNumber:i});
+        }
+      });
+    }
+
+    this.setState({year:newYearNumber, week:1, loadedWeeks:newLoadedWeeks});
+  }
+
+  changeWeek(newWeekNumber){    
+    this.setState({week:newWeekNumber});
+    this.getTerritoriesFromCSV(this.state.year,newWeekNumber);
+    this.forceUpdate();
+    alert("When clicking the appropriate button on the right panel, this should update the territories to be of their state in that week - but for some reason, the DOM doesn't reflect the change when you press the button, even though it's the exact same that activates successfully on page load.");
+    alert("...is it because I'm making an array of react elements when the territory array is generated? Would it be better to store them as objects, then make the components in the render() method - and then it might actually display them?");
   }
 
   getTerritoriesFromCSV(year,week){
@@ -79,6 +115,8 @@ class App extends React.Component {
       
       console.log("Week "+week+": "+ lines[0]);
   
+      let territoriesTemp = [];
+      
       for (let i = 3; i < lines.length; i++){
           let splitLine = lines[i].split(",");
           if (splitLine[0].trim() == "ADJACENCY"){
@@ -90,13 +128,16 @@ class App extends React.Component {
                       splitLine[4].trim(),
                       splitLine[5].trim()));
           } else {
-              this.state.territories.push(<Territory name={splitLine[0].trim()} alignment={splitLine[1].trim().toLowerCase()} holder={splitLine[2].trim()} emPosX={splitLine[3].trim()} emPosY={splitLine[4].trim()}/>);
+            territoriesTemp.push(<Territory name={splitLine[0].trim()} alignment={splitLine[1].trim().toLowerCase()} holder={splitLine[2].trim()} emPosX={splitLine[3].trim()} emPosY={splitLine[4].trim()} week={week}/>);
           }
       }
+      this.setState({territories:territoriesTemp});
     });
     }
   
   render(){
+    let territoriesForDisplay = this.state.territories.map((territory) => <>{territory}</>);
+    console.log(territoriesForDisplay);
     return (
     <>
         {this.state.canvas}        
@@ -106,7 +147,7 @@ class App extends React.Component {
         <div id="display" style={{position:'absolute',left:"calc("+this.state.mapAdjustLeft+" + "+this.state.displayLeft+"px)",
             top:"calc("+this.state.mapAdjustTop+" + "+this.state.displayTop+"px)",fontSize:this.state.fontSizeEm+"em"}}>
             <div id="territories" style={{position:'absolute', left:this.state.mapAdjustLeft, top:this.state.mapAdjustTop}}>
-              <>{this.state.territories.map((territory) => <>{territory}</>)}</>
+              {territoriesForDisplay}
             </div>
           <>{this.state.displayLeft}</>
         </div>
@@ -117,9 +158,7 @@ class App extends React.Component {
             <hr/>
             <br/>
             <div className="weeksScroll">
-                <h4 className="weekOption">Week 1 - New domain, old problems</h4>
-                <h4 className="weekOption">Week 2 - Ghost-al Vote</h4>
-                <h4 className="weekOption">Week 3 - Zoo Fast, Zoo Furious</h4>
+              <>{this.state.loadedWeeks.map((week) => <><h4 className="weekOption" onClick={() => this.state.week != week.weekNumber ? this.changeWeek(week.weekNumber) : null}>{week.title}</h4></>)}</>
             </div>
         </div>
     </>
