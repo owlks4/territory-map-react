c44cb99ccc6a241b9d5dfa01672864df832941fd
owlks4 <115742200+owlks4@users.noreply.github.com>
2023-10-20 19:50:46 +0100

    Panning/zooming works

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+let app = <App year={6} week={3}/>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App year={6} week={3}/>
+    {app}
   </React.StrictMode>,
 )
