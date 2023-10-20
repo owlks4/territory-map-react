a0684a06a5d02071a0efcff85a10a0ea0729f5b1
owlks4 <owilkes250@gmail.com>
2023-10-20 22:37:26 +0100

    Steps towards getting page to register updates when changing weeks

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-let app = <App year={6} week={3}/>;
+let app = <App/>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
