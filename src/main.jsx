b1014452158fc7ac87082530dc8ca8a200f8aae1
owlks4 <owilkes250@gmail.com>
2023-10-22 21:45:16 +0100

    Added some 'loading...' text because the CSV fetching takes slightly longer on mobile

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 let app = <App/>;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <React.StrictMode>    
     {app}
-  </React.StrictMode>,
+  </React.StrictMode>
 )
